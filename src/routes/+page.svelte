e8ab84e7d6b8557d25180f009afb7c935eee0ab1
Michael Enger <git@michaelenger.com>
2023-05-27 17:02:17 +0100

    Add basic layout for index page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,114 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import infinitePromo from '$lib/images/promo-gb_infinite.png';
+
+</script>
+
+<section class="grid">
+	<div>
+		<a id="infinite" href="https://www.giantbomb.com/infinite/">
+			<h2>Giant Bomb Infinite</h2>
+			<p>Watch videos from the archive, 24/7.</p>
+		</a>
+	</div>
+	<div>
+		<a id="infinite" href="/random">
+			<h2>Random</h2>
+			<p>Watch a random video from the archive.</p>
+		</a>
+	</div>
+</section>
+
+<section>
+	<div>
+		<h2>Some List of Videos</h2>
+		&middot;
+		<a href="/TODO">See All</a>
+	</div>
+	<ul class="grid">
+		<li>
+			<img src="http://placekitten.com/320/180" alt="">
+			<strong>Video Title</strong>
+			<time class="secondary">Jul 21, '19</time>
+		</li>
+		<li>
+			<img src="http://placekitten.com/320/180" alt="">
+			<strong>Video Title</strong>
+			<time class="secondary">Jul 21, '19</time>
+		</li>
+		<li>
+			<img src="http://placekitten.com/320/180" alt="">
+			<strong>Video Title</strong>
+			<time class="secondary">Jul 21, '19</time>
+		</li>
+		<li>
+			<img src="http://placekitten.com/320/180" alt="">
+			<strong>Video Title</strong>
+			<time class="secondary">Jul 21, '19</time>
+		</li>
+		<li>
+			<img src="http://placekitten.com/320/180" alt="">
+			<strong>Video Title</strong>
+			<time class="secondary">Jul 21, '19</time>
+		</li>
+	</ul>
+</section>
+
+<section>
+	<div>
+		<h2>Another List of Videos</h2>
+		&middot;
+		<a href="/TODO">See All</a>
+	</div>
+	<ul class="grid">
+		<li>
+			<img src="http://placekitten.com/320/180" alt="">
+			<strong>Video Title</strong>
+			<time class="secondary">Jul 21, '19</time>
+		</li>
+		<li>
+			<img src="http://placekitten.com/320/180" alt="">
+			<strong>Video Title</strong>
+			<time class="secondary">Jul 21, '19</time>
+		</li>
+		<li>
+			<img src="http://placekitten.com/320/180" alt="">
+			<strong>Video Title</strong>
+			<time class="secondary">Jul 21, '19</time>
+		</li>
+		<li>
+			<img src="http://placekitten.com/320/180" alt="">
+			<strong>Video Title</strong>
+			<time class="secondary">Jul 21, '19</time>
+		</li>
+		<li>
+			<img src="http://placekitten.com/320/180" alt="">
+			<strong>Video Title</strong>
+			<time class="secondary">Jul 21, '19</time>
+		</li>
+	</ul>
+</section>
+
+<style>
+	h2 {
+		display: inline;
+		font-size: 1.25rem;
+		margin: 0;
+	}
+
+	ul {
+		padding: 0;
+	}
+
+	li {
+		list-style: none;
+	}
+
+	strong,
+	time {
+		display: block;
+	}
+
+	#infinite {
+
+	}
+</style>
