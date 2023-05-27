ed26cab6abe9fc1e45b97c6ad35c53d1581f209c
Michael Enger <git@michaelenger.com>
2023-05-27 18:00:35 +0100

    Add a basic VideoLink component

---
+++ src/lib/components/VideoLink.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { Video } from '$lib/data'
+
+	export let video: Video
+	export let href: string
+</script>
+
+<a {href}>
+	<img src={video.image} alt="">
+	<strong>{video.title}</strong>
+	<time class="secondary" datetime={video.publicationDate.toISOString()}>{video.publicationDate.toLocaleDateString()}</time>
+</a>
---
+++ src/lib/data.ts
@@ -0,0 +1,7 @@
+export interface Video {
+	id: string,
+	title: string,
+	description: string,
+	image: string,
+	publicationDate: Date,
+}
