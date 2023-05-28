54fda01b257468b52273bc93cc5da3252df9eb2c
Michael Enger <git@michaelenger.com>
2023-05-29 00:06:20 +0100

    Change IA download script to also fetch shows

--- scripts/download_ia_data.py
+++ scripts/download_ia_data.py
@@ -1,10 +1,9 @@
 import json
 import re
 import time
-from os import getenv
 from urllib.parse import quote
 
-from internetarchive import configure, get_files, search_items
+from internetarchive import get_files, search_items
 
 # Identifier for the GB archive
 COLLECTION_IDENTIFIER = 'giant-bomb-archive'
@@ -15,86 +14,105 @@ DELAY_TIME = 2
 # Which thumbnail in the list to use as the image
 NTH_THUMBNAIL = 4
 
-# Filename to write the data to
-TARGET_FILE = 'videos.json'
+# Files to write the data to
+TARGET_VIDEO_FILE = 'videos.json'
+TARGET_SHOW_FILE = 'shows.json'
 
 # Limiting the items to get, leave it as 0 to have no limit
-LIMIT = 1000
+LIMIT = 0
 
 
-def get_show_id(result):
-    """Extract out the show ID from a result by looking at its subject(s)."""
+def make_identifier(name):
+    """Convert a name into an identifier."""
 
-    subjects = result['subject']
-    if type(subjects) == str:
-        subjects = [subjects]
-
-    if 'Giant Bomb' in subjects:
-        subjects.remove('Giant Bomb')
-
-    if len(subjects) != 1:
-        print(f"[{result['identifier']}] Unable to determine show:", subjects)
-        return None
-
-    return re.sub(r'[^\w-]', '', subjects[0].lower().replace(' ', '-'))
+    return re.sub(r'[^\w-]', '', name.lower().replace(' ', '-'))
 
 
-print('Logging in to the Internet Archive')
-configure(getenv('IA_USERNAME'), getenv('IA_PASSWORD'))
-
-print(f'Searching for items in collection: {COLLECTION_IDENTIFIER}')
-search = search_items(
-    query=f'collection:{COLLECTION_IDENTIFIER}',
-    fields=['identifier', 'date', 'title', 'description', 'mediatype', 'subject'],
-)
-
-print('Extracting video items...')
-videos = []
-count = 0
-for result in search:
-    identifier = result['identifier']
-
-    if result['mediatype'] != 'movies':
-        print('{identifier}: Skipping non-movie entry')
-        continue
-
-    try:
-        video = {
-            'id': identifier,
-            'title': result['title'],
-            'description': result.get('description', ''),
-            'date': result['date'],
-            'show': get_show_id(result),
-        }
-    except KeyError:
-        print('{identifier}: Missing metadata:', result)
-        continue
-
-    files = get_files(
-        identifier=identifier,
-        formats='Thumbnail',
+def fetch_ia_data():
+    """Get video items from Internet Archive."""
+    print(f'Searching for items in collection: {COLLECTION_IDENTIFIER}')
+    search = search_items(
+        query=f'collection:{COLLECTION_IDENTIFIER}',
+        fields=['identifier', 'date', 'title', 'description', 'mediatype', 'subject'],
     )
 
-    try:
-        i = 0
-        file = next(files)
-        while i < NTH_THUMBNAIL:
-            video['image'] = f'https://archive.org/download/{identifier}/' + quote(file.name)
+    print('Fetching video items...')
+    videos = []
+    shows = {}
+    count = 0
+    for result in search:
+        video_id = result['identifier']
+
+        if result['mediatype'] != 'movies':
+            print('{video_id}: Skipping non-movie entry')
+            continue
+
+        try:
+            video = {
+                'id': video_id,
+                'title': result['title'],
+                'description': result.get('description', ''),
+                'date': result['date'],
+            }
+
+            subjects = (
+                [result['subject']]
+                if type(result['subject']) == str
+                else result['subject']
+            )
+            for subject in subjects:
+                if subject == 'Giant Bomb':
+                    continue
+
+                show_id = make_identifier(subject)
+                if show_id not in shows:
+                    shows[show_id] = {
+                        'id': show_id,
+                        'title': subject,
+                        'description': '',
+                        'videos': []
+                    }
+
+                shows[show_id]['videos'].append(video_id)
+
+        except KeyError:
+            print('{video_id}: Missing metadata:', result)
+            continue
+
+        files = get_files(
+            identifier=video_id,
+            formats='Thumbnail',
+        )
+
+        try:
+            i = 0
             file = next(files)
-            i += 1
-    except StopIteration:
-        pass  # ran out of thumbnails, so just use the last one
+            while i < NTH_THUMBNAIL:
+                video['thumbnail'] = f'https://archive.org/download/{video_id}/' + quote(file.name)
+                file = next(files)
+                i += 1
+        except StopIteration:
+            pass  # ran out of thumbnails, so just use the last one
 
-    videos.append(video)
+        videos.append(video)
 
-    count = count + 1
-    if count == LIMIT:
-        break
+        count = count + 1
+        if count == LIMIT:
+            break
 
-    time.sleep(DELAY_TIME)
+        time.sleep(DELAY_TIME)
 
-print(f'Got {count} videos!')
+    print(f'Got {count} videos and {len(shows)} shows!')
+    return videos, list(shows.values())
 
-print(f'Saving to: {TARGET_FILE}')
-with open(TARGET_FILE, 'w', encoding='utf-8') as file:
-    json.dump(videos, file, ensure_ascii=False)
+
+if __name__ == '__main__':
+    videos, shows = fetch_ia_data()
+
+    print(f'Saving videos to: {TARGET_VIDEO_FILE}')
+    with open(TARGET_VIDEO_FILE, 'w', encoding='utf-8') as file:
+        json.dump(videos, file, ensure_ascii=False, indent=4)
+
+    print(f'Saving shows to: {TARGET_SHOW_FILE}')
+    with open(TARGET_SHOW_FILE, 'w', encoding='utf-8') as file:
+        json.dump(shows, file, ensure_ascii=False, indent=4)
