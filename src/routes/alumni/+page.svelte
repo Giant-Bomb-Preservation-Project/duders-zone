442300ee4d807ce51ea63f02af8aa6b19e35cc42
Michael Enger <git@michaelenger.com>
2023-05-30 00:53:45 +0100

    Wrap alumni content in container

--- src/routes/alumni/+page.svelte
+++ src/routes/alumni/+page.svelte
@@ -1,3 +1,13 @@
-<h1>Alumni</h1>
+<div class="container">
+	<h1>Alumni</h1>
 
-<p>See you next game.</p>
+	<p>See you next game.</p>
+</div>
+
+
+<style>
+	.container {
+		margin-bottom: 20px;
+		margin-top: 20px;
+	}
+</style>
