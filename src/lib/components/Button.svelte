b39c984daffdeb2db4396c25f448aafab42c4429
Michael Enger <git@michaelenger.com>
2023-05-29 23:49:53 +0100

    Add Button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+	export let handler: () => any
+</script>
+
+<button on:click={handler}>
+	<slot></slot>
+</button>
+
+<style>
+	button {
+		display: inline-block;
+		cursor: pointer;
+		padding: 0;
+		position: relative;
+		border: 0;
+		border-radius: 15px;
+		background-color: #b31919;
+		text-align: center;
+		color: #fff;
+		font-family: inherit;
+		font-weight: bold;
+		padding: 8px 24px;
+		text-shadow: rgba(0,0,0,0.35) 0 1px 1px;
+		box-shadow: rgba(255,255,255,0.15) 0 1px 0 inset,rgba(0,0,0,0.35) 0 1px 3px;
+		background-color: #a11717;
+		background-image: linear-gradient(#c51c1c,rgba(161,23,23,0));
+		background-repeat: repeat-x;
+		transition: all 0.15s;
+	}
+
+	button:active {
+		color: #e33636;
+		background-color: #bc1a1a;
+		background-image: linear-gradient(#aa1818,rgba(188,26,26,0));
+		background-repeat: repeat-x;
+	}
+
+	button::before {
+		content: "";
+		position: absolute;
+		top: -5px;
+		left: -5px;
+		right: -5px;
+		bottom: -5px;
+		z-index: -1;
+		border-radius: 18px;
+		background-color: #fcfcfd;
+		box-shadow: rgba(0,0,0,0.15) 0 1px 3px inset;
+		background-color: #fcfcfd;
+		background-image: linear-gradient(#e7e8e9,rgba(252,252,253,0));
+		background-repeat: repeat-x;
+	}
+</style>
