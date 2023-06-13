553c168bf81947a58cbff7424684ac8d7de360af
Devin Spikowski <vegeta897@gmail.com>
2023-06-12 23:29:46 -0400

    Add pagination to show page

--- src/routes/shows/[show]/[video]/+page.svelte
+++ src/routes/shows/[show]/[video]/+page.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
+	import { page } from '$app/stores'
+	import Pagination, { PAGE_SIZE } from '$lib/components/Pagination.svelte'
 	import VideoEmbed from '$lib/components/VideoEmbed.svelte'
 	import VideoList from '$lib/components/VideoList.svelte'
 	import type { PageData } from './$types'
 
 	export let data: PageData
+
+	$: pageParam = $page.url.searchParams.get('page')
+	$: pageNumber = pageParam ? parseInt(pageParam) : 1
+	$: pageVideoIndexStart = (pageNumber - 1) * PAGE_SIZE
+	$: pageVideos = data.videos.slice(pageVideoIndexStart, pageVideoIndexStart + PAGE_SIZE)
 </script>
 
 <h1 class="sr-only">{data.show.title}</h1>
@@ -13,7 +20,8 @@
 </section>
 
 <section class="container videos">
-	<VideoList videos={data.videos} title={data.show.title} rootUri="/shows/{data.show.id}" />
+	<VideoList videos={pageVideos} title={data.show.title} rootUri="/shows/{data.show.id}" />
+	<Pagination results={data.videos.length} current={pageNumber} />
 </section>
 
 <style>
