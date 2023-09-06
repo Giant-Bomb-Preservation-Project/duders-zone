6a3394db7f676692f833539c7d30549469c2caac
Michael Enger <git@michaelenger.com>
2023-09-06 21:35:54 +0100

    Change search page to be submit-based

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Button from '$lib/components/Button.svelte'
 	import VideoList from '$lib/components/VideoList.svelte'
 	import { searchVideos } from '$lib/data'
 	import type { Video } from '$lib/data'
@@ -8,7 +9,8 @@
 
 	let searchQuery = ''
 
-	const searchChanged = () => {
+	//TODO debounce (trailing edge) this function
+	const searchSubmit = () => {
 		videos = searchQuery === '' ? [] : searchVideos(searchQuery)
 	}
 
@@ -17,28 +19,49 @@
 <div class="container">
 	<h1>Search</h1>
 
-	<form>
+	<form on:submit={searchSubmit}>
 		<label for="search" class="sr-only">Search Query</label>
-		<input type="search" id="search" placeholder="Type search here..." bind:value={searchQuery} on:input={searchChanged}>
+		<div id="field-wrapper">
+			<input type="search" id="search" placeholder="search for something" bind:value={searchQuery}>
+		</div>
+		<Button handler={() => {}}>Go Get It</Button>
 	</form>
 
 	{#if videos.length }
 		<VideoList {videos} title="Videos" />
 	{:else}
-		<div class="empty">No video results...</div>
+		<div class="empty">No videos...</div>
 	{/if}
 
 </div>
 
 <style>
 	form {
+		align-items: center;
+		display: flex;
 		margin: 3em auto;
 		max-width: 400px;
 		width: 90%;
 	}
 
+	#field-wrapper {
+		background-image: linear-gradient(#e8e8e8, #fff);
+		border-radius: 4px;
+		box-shadow: rgba(255,255,255,0.25) 0 1px 0,rgba(0,0,0,0.4) 0 1px 3px inset;
+		flex: 1;
+		height: 25px;
+		margin-right: 1em;
+	}
+
 	input {
+		background: url(/assets/icn_sprite2.png) no-repeat -8px -414px;
+		border: 0;
+		border-radius: 5px;
+		color: black;
 		display: block;
+		height: 100%;
+		padding: 0 0 0 16px;
+		text-shadow: 0 1px 0 rgba(255,255,255,0.5);
 		width: 100%;
 	}
 
