897238fdd100514db4207111d1df10100cf37117
Michael Enger <git@michaelenger.com>
2023-09-06 20:55:01 +0100

    Add function for extracting words from text

---
+++ tests/lib/text.test.ts
@@ -0,0 +1,18 @@
+// sum.test.js
+import { describe, it, expect } from 'vitest'
+import { extractWords } from '$lib/text'
+
+describe('extractWords', () => {
+
+	it('coverts text to a list of words', () => {
+		expect(extractWords('test'))
+			.toStrictEqual(['test'])
+		expect(extractWords('This is a test'))
+			.toStrictEqual(['this', 'is', 'a', 'test'])
+		expect(extractWords('Testing 2: The return of test-hater Tom [REMIX]'))
+			.toStrictEqual(['testing', '2', 'the', 'return', 'of', 'test', 'hater', 'tom', 'remix'])
+		expect(extractWords('The THE: the tHe-thE ThE'))
+			.toStrictEqual(['the'])
+	})
+
+})
