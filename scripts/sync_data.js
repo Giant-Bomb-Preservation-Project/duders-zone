e7254b66476c01bc4f9a3c0c2b96db45e6a3b6ce
Michael Enger <git@michaelenger.com>
2024-03-09 15:36:53 +0000

    Add downloading of image files from GB

--- scripts/sync_data.js
+++ scripts/sync_data.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import commandLineArgs from 'command-line-args'
 import { promises as fs } from 'fs'
+import path from 'path'
 
 // Base URL to the Giant Bomb API
 const BASE_URL = 'https://www.giantbomb.com/api/'
@@ -15,14 +16,14 @@ const SHOWS_PER_PAGE = 100
 const SHOWS_FILE = 'src/lib/data/shows.json'
 
 // Path to the location to store the images
-const IMAGES_PATH = 'static/shows'
+const IMAGES_PATH = 'static/shows/'
 
 // The API key used when connecting to the Giant Bomb API
 const GB_API_KEY = process.env.GB_API_KEY
 
 // Sleep function (thanks to: https://stackoverflow.com/a/39914235)
 function sleep(seconds) {
-    return new Promise(resolve => setTimeout(resolve, seconds * 1000));
+	return new Promise((resolve) => setTimeout(resolve, seconds * 1000))
 }
 
 // Convert a title into a show ID
@@ -43,7 +44,7 @@ function getImageFilename(url) {
 		.split('/')
 		.pop()
 		.replaceAll(' ', '-')
-		.replace(/(.*\.\w+)(.*?)$/, '$1')  // remove anything after the last period
+		.replace(/(.*\.\w+)(.*?)$/, '$1') // remove anything after the last period
 }
 
 // Get a list of all the shows
@@ -59,41 +60,51 @@ async function getShows() {
 		var results = []
 		try {
 			console.debug(`Fetching from: ${full_url}`)
-			const response = await axios.get(full_url);
+			const response = await axios.get(full_url)
 			if (response.status !== 200) {
-	            console.error(`ERROR! Unexpected {response.status} response code`)
-	            console.error(response.data)
-	            return []
-	        }
+				console.error(`ERROR! Unexpected {response.status} response code`)
+				console.error(response.data)
+				return []
+			}
 
-			results = response.data.results ?? [];
+			results = response.data.results ?? []
 		} catch (error) {
-			console.error(error);
+			console.error(error)
 			return []
 		}
 
 		console.debug(` -> ${results.length} results`)
 
 		if (results.length == 0) {
-			break  // nothing to do
+			break // nothing to do
 		}
 
 		for (const result of results) {
 			shows.push({
-                'id': makeIdentifier(result.title),
-                'title': result.title,
-                'description': result.deck,
-                'poster': result.image ? result.image.medium_url : null,
-                'logo': result.logo ? result.logo.medium_url : null,
-            })
+				id: makeIdentifier(result.title),
+				title: result.title,
+				description: result.deck,
+				poster: result.image ? result.image.medium_url : null,
+				logo: result.logo ? result.logo.medium_url : null,
+			})
 		}
 
-        page = page + 1
+		page = page + 1
 
-        await sleep(DELAY_TIME)
+		await sleep(DELAY_TIME)
 	}
 
 	return shows
+}
+
+// Download an image file
+async function downloadFile(source, target) {
+	console.debug(`Downloading: ${source}`)
+	const response = await axios.get(source, { responseType: 'arraybuffer' })
+	const fileData = Buffer.from(response.data, 'binary')
+
+	console.debug(` -> saving to ${target}`)
+	await fs.writeFile(target, fileData)
 }
 
 // Run the script
@@ -124,11 +135,11 @@ async function run() {
 		console.log('Fetching show data...')
 
 		shows = await getShows()
-		console.log(`Got ${shows.length} shows!`)
 
 		if (options.shows) {
+			console.log(`Got ${shows.length} shows!`)
 			console.log(`Saving shows to: ${SHOWS_FILE}`)
-			const formattedShows = shows.map(show => {
+			const formattedShows = shows.map((show) => {
 				show.poster = getImageFilename(show.poster)
 				show.logo = getImageFilename(show.logo)
 				return show
@@ -138,7 +149,28 @@ async function run() {
 	}
 
 	if (options.images) {
-		console.warn("TODO")
+		var images = {}
+		for (const show of shows) {
+			if (show.poster != null) {
+				images[getImageFilename(show.poster)] = show.poster
+			}
+			if (show.logo != null) {
+				images[getImageFilename(show.logo)] = show.logo
+			}
+		}
+
+		console.log(`Downloading ${Object.values(images).length} images...`)
+		for (const image of Object.keys(images)) {
+			const target = IMAGES_PATH + image
+
+			try {
+				await fs.access(target, fs.constants.F_OK)
+				console.debug(`Skipping ${image}`)
+			} catch {
+				await downloadFile(images[image], target)
+				await sleep(DELAY_TIME)
+			}
+		}
 	}
 }
 
