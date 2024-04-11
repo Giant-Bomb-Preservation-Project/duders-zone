44d03577e22931d8706eed06f676230f9f938ebd
Michael Enger <git@michaelenger.com>
2024-04-11 17:54:32 +0100

    Move source selector to sub-menu of video controls

--- src/lib/components/VideoEmbed.svelte
+++ src/lib/components/VideoEmbed.svelte
@@ -37,54 +37,29 @@
 				title={video.title}
 			/>
 		{/if}
-		<div class="source-selector">
-			<span class="sr-only">Source</span>
-			{#if video.source.youtube}
-				<button
-					title="Use YouTube video source"
-					on:click={() => setSource(VideoSource.YouTube)}
-					class={videoSource === VideoSource.YouTube && 'current'}
-				>
-					<svg
-						viewBox="0 -38 256 256"
-						version="1.1"
-						xmlns="http://www.w3.org/2000/svg"
-						xmlns:xlink="http://www.w3.org/1999/xlink"
-						preserveAspectRatio="xMidYMid"
+		<ul class="video-controls">
+			<li>
+				<span class="link link-gear">Source</span>
+				<div class="controls">
+					<button
+						title="Use Internet Archive video source"
+						on:click={() => setSource(VideoSource.InternetArchive)}
+						class={videoSource === VideoSource.InternetArchive && 'current'}
+						disabled={!video.source.internetarchive && 'disabled'}
 					>
-						<g>
-							<path
-								d="M250.346231,28.0746923 C247.358133,17.0320558 238.732098,8.40602109 227.689461,5.41792308 C207.823743,0 127.868333,0 127.868333,0 C127.868333,0 47.9129229,0.164179487 28.0472049,5.58210256 C17.0045684,8.57020058 8.37853373,17.1962353 5.39043571,28.2388718 C-0.618533519,63.5374615 -2.94988224,117.322662 5.5546152,151.209308 C8.54271322,162.251944 17.1687479,170.877979 28.2113844,173.866077 C48.0771024,179.284 128.032513,179.284 128.032513,179.284 C128.032513,179.284 207.987923,179.284 227.853641,173.866077 C238.896277,170.877979 247.522312,162.251944 250.51041,151.209308 C256.847738,115.861464 258.801474,62.1091 250.346231,28.0746923 Z"
-								fill="#FF0000"
-							/>
-							<polygon
-								fill="#FFFFFF"
-								points="102.420513 128.06 168.749025 89.642 102.420513 51.224"
-							/>
-						</g>
-					</svg>
-				</button>
-			{/if}
-			{#if video.source.internetarchive}
-				<button
-					title="Use Internet Archive video source"
-					on:click={() => setSource(VideoSource.InternetArchive)}
-					class={videoSource === VideoSource.InternetArchive && 'current'}
-				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						aria-label="Internet Archive"
-						role="img"
-						viewBox="0 0 512 512"
+						Internet Archive
+					</button>
+					<button
+						title="Use YouTube video source"
+						on:click={() => setSource(VideoSource.YouTube)}
+						class={videoSource === VideoSource.YouTube && 'current'}
+						disabled={!video.source.youtube && 'disabled'}
 					>
-						<rect width="512" height="512" rx="15%" fill="#ffffff" />
-						<path
-							d="m81 419h350v18h-350zm14-34h323v25h-323zm-2-284h321v35h-321zm319-10 10-11-169-39-168 39 10 11h158zm-273 154-1-51-3-47c0-2 0-2-2-2a67 67 0 0 0 -28 0c-1 0-2 0-2 2l-2 47a2223 2223 0 0 0 0 127l2 43 1 8 15 3c6-1 11-1 16-3l1-8 2-43a1616 1616 0 0 0 1-76zm88 0-2-51-2-47c0-2-1-2-2-2a67 67 0 0 0 -28 0c-2 0-2 0-2 2l-3 47a2223 2223 0 0 0 0 127l2 43 1 8c5 2 11 2 16 3l16-3v-8l2-43a1620 1620 0 0 0 2-76zm101 0-1-51-3-47c0-2 0-2-2-2a67 67 0 0 0 -28 0c-1 0-2 0-2 2l-2 47a2223 2223 0 0 0 0 127l2 43 1 8 15 3c5-1 11-1 16-3l1-8 2-43a1624 1624 0 0 0 1-76zm85 0-1-51-2-47c0-2-1-2-2-2a67 67 0 0 0 -29 0l-1 2-3 47a2227 2227 0 0 0 0 127l2 43 1 8c5 2 10 2 16 3l15-3 1-8 2-43a1620 1620 0 0 0 1-76z"
-						/>
-					</svg>
-				</button>
-			{/if}
-		</div>
+						YouTube
+					</button>
+				</div>
+			</li>
+		</ul>
 	</div>
 </Splash>
 
@@ -92,20 +67,6 @@
 	h3 {
 		font-size: 30px;
 		margin: 0;
-	}
-
-	button {
-		background: none;
-		border: 0;
-		cursor: pointer;
-		font-size: 16px;
-		opacity: 0.4;
-		padding: 0 0.15em;
-	}
-
-	button:hover,
-	button.current {
-		opacity: 1;
 	}
 
 	p {
@@ -119,14 +80,107 @@
 		line-height: 20px;
 	}
 
-	.source-selector {
-		font-size: 18px;
-		margin-top: 0.5em;
-		text-align: right;
-	}
-
 	.video {
 		margin: var(--spacing) 0;
+	}
+
+	.video-controls {
+		background: #1d1f20;
+		background-image: linear-gradient(#292b2e, rgba(29, 31, 32, 0));
+		background-repeat: repeat-x;
+		box-shadow: #3c4044 0 1px 0 inset;
+		color: #afb3b6;
+		display: flex;
+		font-family: var(--font-regular);
+		font-weight: bold;
+		font-size: 10px;
+		justify-content: flex-end;
+		margin: 0;
+		list-style: none;
+		padding: 0 5px;
+	}
+
+	.video-controls button {
+		background: none;
+		border: 0;
+		color: inherit;
+		cursor: pointer;
+		display: block;
+		font-size: inherit;
+		font-weight: inherit;
+		margin: 0;
+		padding: 5px;
+		text-align: left;
+	}
+
+	.video-controls button:disabled {
+		opacity: 0.3;
+	}
+
+	.video-controls button:hover,
+	.video-controls button.current {
+		color: #f5faff;
+		text-shadow: #007fff 0 0 3px, #007fff 0 0 5px;
+	}
+
+	.video-controls li {
+		margin: 0 5px;
+		padding: 8px 5px;
+		position: relative;
+	}
+
+	.video-controls li:hover .controls {
+		display: block;
+	}
+
+	.video-controls li:hover .link {
+		color: #f5faff;
+		text-shadow: #007fff 0 0 3px, #007fff 0 0 5px;
+	}
+
+	.video-controls li:hover .link-gear {
+		background-position: left -162px;
+	}
+
+	.video-controls .controls {
+		background-color: #1d1f20;
+		box-shadow: rgba(0, 0, 0, 0.25) 0 0 10px inset, rgba(255, 255, 255, 0.15) 0 1px 0 inset,
+			rgba(0, 0, 0, 0.75) 0 0 5px;
+		border-radius: 3px;
+		border: 1px solid #111213;
+		bottom: 30px;
+		display: none;
+		left: -48px;
+		position: absolute;
+		padding: 5px 10px;
+		width: 100px;
+	}
+
+	.video-controls .controls::before {
+		content: ' ';
+		border-top: 6px solid #111213;
+		border-left: 6px solid transparent;
+		border-right: 6px solid transparent;
+		position: absolute;
+		display: block;
+		top: 100%;
+		left: 50%;
+		margin-left: -5px;
+	}
+
+	.video-controls .link {
+		background: url(/assets/av-sprite@2x.png) no-repeat left center;
+		background-size: 20px auto;
+		cursor: pointer;
+		display: block;
+		height: 16px;
+		line-height: 16px;
+		padding-left: 20px;
+		overflow: hidden;
+	}
+
+	.video-controls .link-gear {
+		background-position: left -142px;
 	}
 
 	@media (min-width: 992px) {
