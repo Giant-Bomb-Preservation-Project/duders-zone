2dbc4b197f433a0ead185220570fb902f567ae53
Michael Enger <git@michaelenger.com>
2024-04-11 17:57:16 +0100

    Persist the preferred video source

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -2,4 +2,8 @@ import { writable } from 'svelte/store'
 
 import { VideoSource } from '$lib/data'
 
-export const preferredSource = writable(VideoSource.InternetArchive)
+export const preferredSource = writable(
+	localStorage.getItem('preferredSource') || VideoSource.InternetArchive
+)
+
+preferredSource.subscribe((val) => localStorage.setItem('preferredSource', val))
