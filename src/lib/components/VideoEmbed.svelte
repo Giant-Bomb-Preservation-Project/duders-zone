cf8b6af43f84b339612e40f55ce5b8baf1866d5a
Michael Enger <git@michaelenger.com>
2024-04-11 13:21:25 +0100

    Add way to toggle between IA and YT video source

--- src/lib/components/VideoEmbed.svelte
+++ src/lib/components/VideoEmbed.svelte
@@ -27,6 +27,53 @@
 		{:else}
 			<p class="error">No video available</p>
 		{/if}
+		<div class="source-selector">
+			<span class="sr-only">Source</span>
+			{#if video.source.youtube}
+				<label title="Use YouTube video source">
+					<input type="radio" name="source" value="youtube" bind:group={videoSource} />
+					<svg
+						viewBox="0 -38 256 256"
+						version="1.1"
+						xmlns="http://www.w3.org/2000/svg"
+						xmlns:xlink="http://www.w3.org/1999/xlink"
+						preserveAspectRatio="xMidYMid"
+					>
+						<g>
+							<path
+								d="M250.346231,28.0746923 C247.358133,17.0320558 238.732098,8.40602109 227.689461,5.41792308 C207.823743,0 127.868333,0 127.868333,0 C127.868333,0 47.9129229,0.164179487 28.0472049,5.58210256 C17.0045684,8.57020058 8.37853373,17.1962353 5.39043571,28.2388718 C-0.618533519,63.5374615 -2.94988224,117.322662 5.5546152,151.209308 C8.54271322,162.251944 17.1687479,170.877979 28.2113844,173.866077 C48.0771024,179.284 128.032513,179.284 128.032513,179.284 C128.032513,179.284 207.987923,179.284 227.853641,173.866077 C238.896277,170.877979 247.522312,162.251944 250.51041,151.209308 C256.847738,115.861464 258.801474,62.1091 250.346231,28.0746923 Z"
+								fill="#FF0000"
+							/>
+							<polygon
+								fill="#FFFFFF"
+								points="102.420513 128.06 168.749025 89.642 102.420513 51.224"
+							/>
+						</g>
+					</svg>
+				</label>
+			{/if}
+			{#if video.source.internetarchive}
+				<label title="Use Internet Archive video source">
+					<input
+						type="radio"
+						name="source"
+						value="internetarchive"
+						bind:group={videoSource}
+					/>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						aria-label="Internet Archive"
+						role="img"
+						viewBox="0 0 512 512"
+					>
+						<rect width="512" height="512" rx="15%" fill="#ffffff" />
+						<path
+							d="m81 419h350v18h-350zm14-34h323v25h-323zm-2-284h321v35h-321zm319-10 10-11-169-39-168 39 10 11h158zm-273 154-1-51-3-47c0-2 0-2-2-2a67 67 0 0 0 -28 0c-1 0-2 0-2 2l-2 47a2223 2223 0 0 0 0 127l2 43 1 8 15 3c6-1 11-1 16-3l1-8 2-43a1616 1616 0 0 0 1-76zm88 0-2-51-2-47c0-2-1-2-2-2a67 67 0 0 0 -28 0c-2 0-2 0-2 2l-3 47a2223 2223 0 0 0 0 127l2 43 1 8c5 2 11 2 16 3l16-3v-8l2-43a1620 1620 0 0 0 2-76zm101 0-1-51-3-47c0-2 0-2-2-2a67 67 0 0 0 -28 0c-1 0-2 0-2 2l-2 47a2223 2223 0 0 0 0 127l2 43 1 8 15 3c5-1 11-1 16-3l1-8 2-43a1624 1624 0 0 0 1-76zm85 0-1-51-2-47c0-2-1-2-2-2a67 67 0 0 0 -29 0l-1 2-3 47a2227 2227 0 0 0 0 127l2 43 1 8c5 2 10 2 16 3l15-3 1-8 2-43a1620 1620 0 0 0 1-76z"
+						/>
+					</svg>
+				</label>
+			{/if}
+		</div>
 	</div>
 </Splash>
 
@@ -34,6 +81,21 @@
 	h3 {
 		font-size: 30px;
 		margin: 0;
+	}
+
+	input[type='radio'] {
+		display: none;
+	}
+
+	label {
+		cursor: pointer;
+		opacity: 0.4;
+		padding: 0 0.15em;
+	}
+
+	label:hover,
+	label:has(input[type='radio']:checked) {
+		opacity: 1;
 	}
 
 	p {
@@ -50,6 +112,12 @@
 	.error {
 		padding: 3em 0;
 		text-align: center;
+	}
+
+	.source-selector {
+		font-size: 18px;
+		margin-top: 0.5em;
+		text-align: right;
 	}
 
 	.video {
