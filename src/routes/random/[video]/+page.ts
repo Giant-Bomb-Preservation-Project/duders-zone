b4bbe65024fd51bf1846f2ea4d145e6cf9fac521
Michael Enger <git@michaelenger.com>
2024-11-01 11:57:06 +0000

    Fix issue where selecting a random video randomises all the videos

--- src/routes/random/[video]/+page.ts
+++ src/routes/random/[video]/+page.ts
@@ -4,9 +4,8 @@ import type { PageLoad } from './$types'
 
 export const load = (({ params }) => {
 	const video = dataStore.getVideoById(params.video)
-	const videos = dataStore.getRandomVideos(12)
 
 	if (video === null) throw error(404, 'Not found')
 
-	return { video, videos }
+	return { video }
 }) satisfies PageLoad
