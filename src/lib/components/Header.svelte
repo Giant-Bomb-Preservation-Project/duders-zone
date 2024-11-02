5b3a53f049d45d682d4c741c13d1681526f35b92
Michael Enger <git@michaelenger.com>
2024-11-02 22:44:52 +0000

    Add a Header component

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte'
+
+	interface Props {
+		children: Snippet
+		title: string
+	}
+
+	const { children, title }: Props = $props()
+</script>
+
+<div class="header-wrapper">
+	<div class="header">
+		<h2>{title}</h2>
+		{@render children()}
+	</div>
+</div>
+
+<style>
+	h2 {
+		font-size: 14px;
+		line-height: 20px;
+		margin: 0;
+	}
+
+	.header {
+		display: flex;
+		font-family: var(--font-special);
+		font-size: 14px;
+		line-height: 20px;
+		justify-content: flex-start;
+	}
+
+	.header-wrapper::after,
+	.header-wrapper::before {
+		content: ' ';
+		display: block;
+		height: 2px;
+		margin-bottom: 6px;
+		background-position: bottom;
+		background-image: url(/assets/bg-border-light.png);
+		background-repeat: repeat-x;
+	}
+
+	.header-wrapper::after {
+		margin-bottom: 0;
+		margin-top: 6px;
+	}
+
+	@media (min-width: 768px) {
+		h2 {
+			font-size: 18px;
+			line-height: 20px;
+		}
+
+		.header {
+			font-size: 18px;
+			line-height: 20px;
+		}
+	}
+</style>
--- src/lib/components/VideoList.svelte
+++ src/lib/components/VideoList.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { Video } from '$lib/data'
 	import { VideoListMode } from '$lib/types'
+	import Header from '$lib/components/Header.svelte'
 	import Thumbnail from '$lib/components/Thumbnail.svelte'
 	import { videoListMode } from '$lib/store.js'
 
@@ -16,50 +17,41 @@
 	const { videos, title, rootUri, seeAllUrl, mode, linkSuffix = '' }: Props = $props()
 </script>
 
-<div class="header-wrapper">
-	<div class="header">
-		<h2>{title}</h2>
-		{#if seeAllUrl}
-			<div class="see-all">&middot; <a href={seeAllUrl}>See All</a></div>
-		{/if}
-		{#if !mode}
-			<div class="controls">
-				<button
-					class={$videoListMode == VideoListMode.List ? 'active' : ''}
-					onclick={() => {
-						videoListMode.set(VideoListMode.List)
-					}}
+<Header {title}>
+	{#if seeAllUrl}
+		<div class="see-all">&middot; <a href={seeAllUrl}>See All</a></div>
+	{/if}
+	{#if !mode}
+		<div class="controls">
+			<button
+				class={$videoListMode == VideoListMode.List ? 'active' : ''}
+				onclick={() => {
+					videoListMode.set(VideoListMode.List)
+				}}
+			>
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor"
+					><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
+						d="M40 48C26.7 48 16 58.7 16 72v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V72c0-13.3-10.7-24-24-24H40zM192 64c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zM16 232v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V232c0-13.3-10.7-24-24-24H40c-13.3 0-24 10.7-24 24zM40 368c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V392c0-13.3-10.7-24-24-24H40z"
+					/></svg
 				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 512 512"
-						fill="currentColor"
-						><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
-							d="M40 48C26.7 48 16 58.7 16 72v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V72c0-13.3-10.7-24-24-24H40zM192 64c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zM16 232v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V232c0-13.3-10.7-24-24-24H40c-13.3 0-24 10.7-24 24zM40 368c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V392c0-13.3-10.7-24-24-24H40z"
-						/></svg
-					>
-					List
-				</button>
-				<button
-					class={$videoListMode == VideoListMode.Grid ? 'active' : ''}
-					onclick={() => {
-						videoListMode.set(VideoListMode.Grid)
-					}}
+				List
+			</button>
+			<button
+				class={$videoListMode == VideoListMode.Grid ? 'active' : ''}
+				onclick={() => {
+					videoListMode.set(VideoListMode.Grid)
+				}}
+			>
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor"
+					><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
+						d="M448 96V224H288V96H448zm0 192V416H288V288H448zM224 224H64V96H224V224zM64 288H224V416H64V288zM64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64z"
+					/></svg
 				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 512 512"
-						fill="currentColor"
-						><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
-							d="M448 96V224H288V96H448zm0 192V416H288V288H448zM224 224H64V96H224V224zM64 288H224V416H64V288zM64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64z"
-						/></svg
-					>
-					Grid
-				</button>
-			</div>
-		{/if}
-	</div>
-</div>
+				Grid
+			</button>
+		</div>
+	{/if}
+</Header>
 
 <ul class={mode ?? $videoListMode}>
 	{#each videos as video}
@@ -81,12 +73,6 @@
 </ul>
 
 <style>
-	h2 {
-		font-size: 14px;
-		line-height: 20px;
-		margin: 0;
-	}
-
 	h3 {
 		font-size: 21px;
 		line-height: 25px;
@@ -145,32 +131,8 @@
 		color: var(----color-gray);
 	}
 
-	.header {
-		display: flex;
-		font-family: var(--font-special);
-		font-size: 14px;
-		line-height: 20px;
-		justify-content: flex-start;
-	}
-
-	.header .see-all {
+	.see-all {
 		margin-left: 0.35em;
-	}
-
-	.header-wrapper::after,
-	.header-wrapper::before {
-		content: ' ';
-		display: block;
-		height: 2px;
-		margin-bottom: 6px;
-		background-position: bottom;
-		background-image: url(/assets/bg-border-light.png);
-		background-repeat: repeat-x;
-	}
-
-	.header-wrapper::after {
-		margin-bottom: 0;
-		margin-top: 6px;
 	}
 
 	.metadata {
@@ -216,11 +178,6 @@
 		.controls {
 			display: block;
 			margin-left: auto;
-		}
-
-		.header {
-			font-size: 18px;
-			line-height: 20px;
 		}
 
 		.thumbnail {
