0ecb9fba9a3688fe33d31de5363b73994b4e3399
Michael Enger <git@michaelenger.com>
2024-11-04 22:44:53 +0000

    Add a link to the infinite stream to the sidebar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,7 +16,7 @@
 	const mainVideo = videos.pop()
 </script>
 
-<section>
+<section class="splash">
 	<Splash image={promoInfinite}>
 		<div class="metadata">
 			<h3>Giant Bomb Infinite</h3>
@@ -45,10 +45,29 @@
 		</section>
 	</div>
 	<aside class="right">
+		<a class="infinite" href="https://www.giantbomb.com/infinite/">
+			<img src={promoInfinite} alt="Giant Bomb Infinite" />
+			<hgroup>
+				<h3>
+					Giant Bomb Infinite
+					<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path
+							d="M10.0002 5H8.2002C7.08009 5 6.51962 5 6.0918 5.21799C5.71547 5.40973 5.40973 5.71547 5.21799 6.0918C5 6.51962 5 7.08009 5 8.2002V15.8002C5 16.9203 5 17.4801 5.21799 17.9079C5.40973 18.2842 5.71547 18.5905 6.0918 18.7822C6.5192 19 7.07899 19 8.19691 19H15.8031C16.921 19 17.48 19 17.9074 18.7822C18.2837 18.5905 18.5905 18.2839 18.7822 17.9076C19 17.4802 19 16.921 19 15.8031V14M20 9V4M20 4H15M20 4L13 11"
+							stroke="currentColor"
+							stroke-width="2"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						/>
+					</svg>
+				</h3>
+				<p>Watch videos from the archive, 24/7.</p>
+			</hgroup>
+		</a>
+
 		<Header title="Random Shows">
 			<div class="see-all">&middot; <a href="/shows">See All</a></div>
 		</Header>
-		<ul class="shows">
+		<ul id="shows">
 			{#each data.shows as show}
 				<li>
 					<a href="/shows/{show.id}">
@@ -66,14 +85,8 @@
 
 <style>
 	h3 {
-		font-size: 30px;
+		font-size: 24px;
 		margin: 0;
-	}
-
-	p {
-		font-size: 14px;
-		line-height: 20px;
-		margin: 10px 0;
 	}
 
 	.columns {
@@ -91,34 +104,93 @@
 		margin-left: var(--spacing);
 	}
 
-	.link {
-		margin: 0;
-	}
-
-	.link a {
-		box-shadow: rgba(255, 255, 255, 0.1) 0 0 0 1px;
-		display: block;
-		line-height: 0;
-	}
-
-	.shows a {
+	.infinite {
+		border: 1px solid black;
+		box-shadow: rgba(0, 0, 0, 0.5) 0 2px 5px;
 		color: white;
 		display: block;
+		margin-bottom: var(--spacing);
 		position: relative;
 	}
 
-	.shows a h3 {
-		font-size: 24px;
+	.infinite h3 {
 		text-shadow: rgba(0, 0, 0, 0.75) 0 1px 1px;
 	}
 
-	.shows a:hover h3 {
+	.infinite:hover h3 {
 		text-shadow:
 			rgba(255, 255, 255, 0.75) 0 0 10px,
 			rgba(0, 0, 0, 0.75) 0 1px 1px;
 	}
 
-	.shows h3 {
+	.infinite hgroup {
+		align-items: start;
+		background: linear-gradient(
+			0deg,
+			rgba(0, 0, 0, 1) 0%,
+			rgba(0, 0, 0, 0) 60%,
+			rgba(0, 0, 0, 0) 100%
+		);
+		bottom: 0;
+		display: flex;
+		flex-direction: column;
+		justify-content: end;
+		left: 0;
+		line-height: 30px;
+		margin: 0;
+		padding: 1em;
+		position: absolute;
+		right: 0;
+		top: 0;
+		z-index: 1;
+	}
+
+	.infinite svg {
+		font-size: 12pt;
+		left: -4px;
+		position: relative;
+		top: -4px;
+	}
+
+	.splash h3 {
+		font-size: 30px;
+	}
+
+	.splash p {
+		font-size: 14px;
+		line-height: 20px;
+		margin: 10px 0;
+	}
+
+	.splash .link {
+		margin: 0;
+	}
+
+	.splash .link a {
+		box-shadow: rgba(255, 255, 255, 0.1) 0 0 0 1px;
+		display: block;
+		line-height: 0;
+	}
+
+	#shows a {
+		border: 1px solid white;
+		box-shadow: rgba(0, 0, 0, 0.5) 0 2px 5px;
+		color: white;
+		display: block;
+		position: relative;
+	}
+
+	#shows a h3 {
+		text-shadow: rgba(0, 0, 0, 0.75) 0 1px 1px;
+	}
+
+	#shows a:hover h3 {
+		text-shadow:
+			rgba(255, 255, 255, 0.75) 0 0 10px,
+			rgba(0, 0, 0, 0.75) 0 1px 1px;
+	}
+
+	#shows h3 {
 		align-items: start;
 		background: linear-gradient(
 			0deg,
@@ -140,12 +212,12 @@
 		z-index: 1;
 	}
 
-	.shows img {
+	#shows img {
 		box-shadow: var(--box-shadow);
 		width: 100%;
 	}
 
-	.shows li {
+	#shows li {
 		margin: var(--spacing) 0;
 	}
 
@@ -156,14 +228,21 @@
 	}
 
 	@media (min-width: 768px) {
-		h3 {
+		.splash h3 {
 			font-size: 32px;
 			line-height: 35px;
 		}
 
-		p {
+		.splash p {
 			font-size: 21px;
 			line-height: 30px;
+		}
+
+		.splash .link {
+			margin: 0 0 0 32px;
+			flex: 0 0 620px;
+			position: relative;
+			z-index: 2;
 		}
 
 		.columns {
@@ -178,17 +257,10 @@
 			position: relative;
 			z-index: 2;
 		}
-
-		.link {
-			margin: 0 0 0 32px;
-			flex: 0 0 620px;
-			position: relative;
-			z-index: 2;
-		}
 	}
 
 	@media (min-width: 1200px) {
-		.link {
+		.splash .link {
 			margin-left: 90px;
 		}
 	}
