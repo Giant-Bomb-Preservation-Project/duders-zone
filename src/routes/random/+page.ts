aa41cfa3dd826661f53e9e207987b4c36b3721b2
Michael Enger <git@michaelenger.com>
2025-04-24 19:55:34 +0100

    Change random page to not show a main video
    Having a video be the _main_ one with the links to the other videos also going to the random page meant that it could generate an infinite amount of pages. This way the links will always go to the actual show/video page.

--- src/routes/random/+page.ts
+++
@@ -1,9 +0,0 @@
-import { error, redirect } from '@sveltejs/kit'
-import { dataStore } from '$lib/data'
-import type { PageLoad } from './$types'
-
-export const load = (({ params }) => {
-	const video = dataStore.getRandomVideos(1)[0]
-
-	redirect(302, `/random/${video.id}`)
-}) satisfies PageLoad
