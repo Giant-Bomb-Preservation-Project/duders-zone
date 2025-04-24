ad1bd766c9a6197d043271218a8d0ab2f8cf0bf9
Michael Enger <git@michaelenger.com>
2025-04-24 01:11:19 +0100

    Move search query handling to a separate TS file
    Was necessary as you can't get the searchParams on static pages.

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
-	import { page } from '$app/stores'
 	import Button from '$lib/components/Button.svelte'
 	import VideoList from '$lib/components/VideoList.svelte'
-	import { dataStore } from '$lib/data'
-	import type { Video } from '$lib/data'
+	import type { PageData } from './$types'
 
-	let searchQuery: string | null = $derived($page.url.searchParams.get('q'))
-	let videos: Video[] = $derived(searchQuery ? dataStore.searchVideos(searchQuery) : [])
+	interface Props {
+		data: PageData
+	}
+
+	const { data }: Props = $props()
+	const { searchQuery, videos } = $derived(data)
 </script>
 
 <div class="container">
