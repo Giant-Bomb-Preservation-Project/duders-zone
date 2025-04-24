bd8f27b1929b4343b9576bcbf7fbeb8764c98f87
Michael Enger <git@michaelenger.com>
2025-04-24 01:10:01 +0100

    Fix issue with localStorage not being available in Node
    Node is used when generating the site so this would throw an error.

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -3,13 +3,31 @@ import { writable } from 'svelte/store'
 import { VideoSource } from '$lib/data'
 import { VideoListMode } from '$lib/types'
 
+// Create a store for browsers
+const createBrowserStore = (key: string, defaultValue: any) => {
+	const store = writable(localStorage.getItem(key) || defaultValue)
+
+	// Store the token in LocalStorage whenever it´s updated
+	store.subscribe((val) => {
+		if (!localStorage) return
+		if (!val) return localStorage.removeItem(key)
+		localStorage.setItem(key, val)
+	})
+
+	return store
+}
+
+// Create a store for Node (only used when building the site)
+const createNodeStore = (defaultValue: any) => writable(defaultValue)
+
 // The preferred video source
-export const preferredSource = writable(
-	localStorage.getItem('preferredSource') || VideoSource.InternetArchive
-)
+export const preferredSource =
+	typeof localStorage === 'undefined'
+		? createNodeStore(VideoSource.InternetArchive)
+		: createBrowserStore('preferredSource', VideoSource.InternetArchive)
 
 // How to show the video list
-export const videoListMode = writable(localStorage.getItem('videoListMode') || VideoListMode.List)
-
-preferredSource.subscribe((val) => localStorage.setItem('preferredSource', val))
-videoListMode.subscribe((val) => localStorage.setItem('videoListMode', val))
+export const videoListMode =
+	typeof localStorage === 'undefined'
+		? createNodeStore(VideoListMode.List)
+		: createBrowserStore('videoListMode', VideoListMode.List)
