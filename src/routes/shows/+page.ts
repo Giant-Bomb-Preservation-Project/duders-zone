0e551fa5afd6ff721f278c241ca3f3aae7da0f3c
Michael Enger <git@michaelenger.com>
2025-12-17 17:41:32 +0000

    Remove the URL redirects

--- src/routes/shows/+page.ts
+++
@@ -1,7 +0,0 @@
-import { redirect } from '@sveltejs/kit'
-import { base } from '$app/paths'
-import type { PageLoad } from './$types'
-
-export const load = (() => {
-	redirect(301, `${base}/videos/shows`)
-}) satisfies PageLoad
